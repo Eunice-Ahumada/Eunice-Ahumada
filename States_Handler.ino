618ab21a71170f2895b13d76d71a3886594eb65f
Eunice Lopez Ahumada <eunicelopezahumada@gmail.com>
2024-05-07 08:45:32 -0600

    Actualizacion

--- States_Handler.ino
+++ States_Handler.ino
@@ -1,22 +1,31 @@
-void handleEvent(Event event) {
-  switch (currentState) {
+#include "Incubator-CO2.ino"
+
+void handleEvent(Event event) { // Takes an event as argument
+  switch (currentState) { // Evaluates the current state of the system
     
     case IDLE:
-      if (event == TIMER_EXPIRED) {
-        currentState = READING_SENSOR;
+      if (event == TIMER_EXPIRED) { // If the event is TIMER_EXPIRED, the current system state is updated to READING_SENSOR
+        currentState = READING_SENSOR; 
         timeToReadSensor();
-      } else if (event == TIME_TO_READ_RTC) {
+      } else if (event == TIME_TO_READ_RTC) { // If the event is not TIMER_EXPIRED, it is checked if it is TIME_TO_READ_RTC
         currentState = READING_RTC;
-        timeToReadRTC();
+        timeToReadRTC(); // Perform the action associated with this event
       }
       break;
 
-    case READING_RTC:
-      if (event == SENSOR_READING_READY) {
+   case READING_RTC:
+     if (event == TIME_TO_READ_SENSOR) {
         currentState = READING_SENSOR;
-        sensorReadingReady();
-      }
-      break;
+        timeToReadSensor();
+     }  else if (event == TIME_TO_READ_RTC) {
+        currentState = IDLE;
+        timeToReadRTC();
+     }  else if (event == TIME_TO_UPDATE_RTC) {
+        currentState = IDLE;
+        timeToUpdateRTC();
+     }
+     break;
+
 
     case UPDATING_RTC:
       if (event == TIME_TO_UPDATE_RTC) {
@@ -25,19 +34,18 @@ void handleEvent(Event event) {
       }
       break;
 
+
     case READING_SENSOR:
-      if (event == CO2_MEASUREMENT) {
+      if (event == TIME_TO_READ_SENSOR) {
+        currentState = CONTROLLING_CO2;
+        timeToReadSensor();
+      } else if (event == CO2_MEASUREMENT) {
         currentState = CONTROLLING_CO2;
         CO2Measurement();
-      } else if (event == CO2_CONTROL) {
-        CO2Control();
-      } else if (event == DATA_LOGGING) {
+      } else if (event == SENSOR_READING_READY) {
         currentState = LOGGING_DATA;
-        dataLogging();
-      } else if (event == CLOUD_SEND) {
-        currentState = SENDING_TO_CLOUD;
-        cloudSend();
-      }
+        sensorReadingReady();
+      }  
       break;
 
     case CONTROLLING_CO2:
